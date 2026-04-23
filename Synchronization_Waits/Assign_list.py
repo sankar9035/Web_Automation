71295544cab82569e1a3e178b043857e95f02640
sankar9035 <sankarrudrapal@gmail.com>
2026-04-23 13:20:10 +0530

    List_validation

---
+++ Synchronization_Waits/Assign_list.py
@@ -0,0 +1,65 @@
+import time
+
+from selenium import webdriver #this is for drivers class.
+from selenium.webdriver.chrome.service import Service #this is for chrome/edge service class.
+from selenium.webdriver.common.by import By
+from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+Original = ['Cucumber - 1 Kg', 'Raspberry - 1/4 Kg', 'Strawberry - 1/4 Kg']
+Actual_list = []
+# service_obj = Service(executable_path=r'C:\Users\sanka\Downloads\edgedriver_win64\msedgedriver.exe')
+# driver = webdriver.Edge(service=service_obj)
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+driver.maximize_window()
+Title = driver.title
+print(Title)
+assert "GreenKart" in Title
+
+driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
+time.sleep(3)
+#-------------------------------------------------------
+# list of webelements add to cart buttons
+#-------------------------------------------------------
+
+
+results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+count = len(results)
+print(count)
+
+assert count > 0
+for result in results:
+    result.find_element(By.XPATH, "div/button").click()
+    Actual_list.append(result.find_element(By.XPATH, "h4").text)
+
+print(Actual_list)
+assert Actual_list == Original
+'''
+chaining methods :
+
+so far results hold 3 elements out gole is to click on add to cart button for all 3 items.
+using chaining mechanism we can achieve this.
+'''
+
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("sankarselenium")
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+'''
+here below line of code going to apply explicit wait since while entering a promo code its take a few min of time,
+that might be not initially applicable for all the elements only for a specific element we can apply explicit wait.
+
+'''
+
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".promoInfo")))
+promo = driver.find_element(By.CSS_SELECTOR, ".promoInfo").text
+print(promo)
+assert "Invalid" in driver.find_element(By.CSS_SELECTOR, ".promoInfo").text
--- Synchronization_Waits/Fun_validation.py
+++ Synchronization_Waits/Fun_validation.py
@@ -11,6 +11,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 # driver = webdriver.Edge(service=service_obj)
 
 driver = webdriver.Chrome()
+#----------------------------------------------
+#We can apply explicit wait for a specific element which is going to appear after applying the promo code.
+#----------------------------------------------
 driver.implicitly_wait(10)
 
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
@@ -25,23 +28,25 @@ time.sleep(3)
 results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
 count = len(results)
 print(count)
-
-assert count > 0
-for result in results:
-    result.find_element(By.XPATH, "div/button").click()
 '''
 chaining methods :
 
 so far results hold 3 elements out gole is to click on add to cart button for all 3 items.
 using chaining mechanism we can achieve this.
 '''
+assert count > 0
+for result in results:
+    result.find_element(By.XPATH, "div/button").click()
 
+#------------------------------------------------
+# Click to add to cart button
+#------------------------------------------------
 driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
 #------------------------------------------------
 # Click to process to checkout button
 #------------------------------------------------
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
-prices = driver.find_elements(By.CSS_SELECTOR, "tr td:nth-child(5) p") #here we target add to cart item prices.
+prices = driver.find_elements(By.CSS_SELECTOR, "tr td:nth-child(5) p") #Here we target add to cart item prices.
 
 #--------------------------------------------------------
 # Sum Validation
@@ -55,21 +60,15 @@ print(sum)
 total = int(driver.find_element(By.CSS_SELECTOR, ".totAmt").text)
 assert sum == total
 
-#---------------------------------------
-#Promocod Apply
-#------------------------------------------
-
-
-#----------------------------------------------
-#1. we can apply explicit wait for a specific element which is going to appear after applying the promo code.
-#----------------------------------------------
 '''
 here below line of code going to apply explicit wait since while entering a promo code its take a few min of time,
 that might be not initially applicable for all the elements only for a specific element we can apply explicit wait.
 So the requirement is once apply a promo_code its will take some time either its a valid code or not,
 and text will be appear in the page after applying the promo code, so we have to wait until that text is visible in the page.
 '''
-
+#---------------------------------------
+#Promocod Apply
+#---------------------------------------
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("sankarselenium")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
 wait = WebDriverWait(driver, 10)
@@ -77,7 +76,9 @@ wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, "
 promo = driver.find_element(By.CSS_SELECTOR, ".promoInfo").text
 print(promo)
 assert "Invalid" in driver.find_element(By.CSS_SELECTOR, ".promoInfo").text
-
+#-------------------------------------------------------
+# Price and total both are having a same class attribute and value, so we have to use CSS selector with nth-child to differentiate between them.
+#-------------------------------------------------------
 discount_price = int(driver.find_element(By.CSS_SELECTOR, ".discountAmt").text)
 assert discount_price == total
 
@@ -86,9 +87,7 @@ assert discount_price == total
 '''
 Here will do a validation before place an order, 
 '''
-#-------------------------------------------------------
-# Price and total both are having a same class attribute and value, so we have to use CSS selector with nth-child to differentiate between them.
-#-------------------------------------------------------
+
 #tr td:nth-child(5) p
 
 
